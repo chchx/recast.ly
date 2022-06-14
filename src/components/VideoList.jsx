16a43100c37187e9d9029ecea5eeec1b8535c878
chrisxchoi <chris@choi.is>
2022-06-14 10:49:09 -0700

    Save changes

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,12 +1,17 @@
-var VideoList = () => (
-  <div className="video-list">
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-  </div>
-);
+import exampleVideoData from '../data/exampleVideoData.js';
+import VideoListEntry from './VideoListEntry.js'
+
+const VideoList = ( {videos, handleVideoListClick} ) => {
+  console.log(videos);
+  return (
+      <ul>
+        {videos.map((video) => (
+        <VideoListEntry handleVideoListClick={handleVideoListClick} video={video}/>
+        ))}
+
+      </ul>
+  );
+};
 
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
